666ebb231f046d14bd7ca2ab81c022c57c98d311
Edgar Gutierrez <edgargutierrez130@gmail.com>
2019-02-21 01:41:16 -0800

    Implemented the Hold Function and DRY principle to the code

--- Final-Project/pig-game.js
+++ Final-Project/pig-game.js
@@ -2,7 +2,7 @@
 GAME RULES:
 
 - The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- In each turn, a player rolls a dice as many times as he wishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
@@ -13,19 +13,21 @@ var scores, roundScore, activePlayer;
 
 scores = [0,0]; // created array
 roundScore = 0;
-activePlayer = 1; // keeps track of player that is playing
+activePlayer = 0; // keeps track of player that is playing
 
 // Targeting css class, using .style.display
 document.querySelector('.dice').style.display = 'none';
 
-/*
-function btn() {
-  // Do something here
-}
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
 // This is one way of interacting with the addEventListener
-document.querySelector('.btn-roll').addEventListener('click', btn);
-*/
-//or you can pass in an anonymous function
+//document.querySelector('.btn-roll').addEventListener('click', btn);
+// OR
+// you can pass in an anonymous function
 // anonymous function is a function that doesn't have a name so it can NOT be reused, which is what we want in our case.
 document.querySelector('.btn-roll').addEventListener('click', function() {
 
@@ -41,9 +43,61 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 
     //3. Update the round score IF the rolled number was NOT a 1
+
+    if (dice !== 1) {
+      //Add score
+      //Same as: roundScore = roundScore + dice
+      roundScore += dice;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+      //Next Player
+      /*Same as: if(activePlayer === 0) {
+        activePlayer = 1;
+    } else {
+        activePlayer = 0;
+    } */
+      //Next Player
+      nextPlayer();
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    //1. Add CURRENT score to GLOBAL score
+    scores[activePlayer] += roundScore;
+
+    //2. Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    //3. Check if Player won the game
+    if (scores[activePlayer] >= 20) {
+      document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+      document.querySelector('.dice').style.display = 'none';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+      //Next Player
+      nextPlayer();
+    }
 });
 
 
+function nextPlayer() {
+  //Next Player
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; // Ternary
+  roundScore = 0;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  //took bottom code off because toggle is the way to go
+  //document.querySelector('.player-0-panel').classList.remove('active');
+  //document.querySelector('.player-1-panel').classList.add('active');
+
+  document.querySelector('.dice').style.display = 'none';
+}
 
 
 
@@ -51,10 +105,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 
 
-
-
-
-// document.querySelector('#current-' + activePlayer).textContent = dice; //set a value
+// dice; //set a value
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 // var x = document.querySelector('#score-0').textContent; // reads value
 // console.log(x);
